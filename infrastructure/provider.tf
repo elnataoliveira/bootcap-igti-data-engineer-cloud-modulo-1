a3b47f827bdbfbdbe8d239e94a9f2a00425d4d5a
elnataoliveira <elnataoliveira@gmail.com>
2021-08-15 12:02:16 -0300

    alter file provider

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "${var.aws_region}"
+    region = var.aws_region
 }
 
 #controle de estado 
 terraform {
   backend "s3" {
-    bucket = "${var.base_bucket_name}"
+    bucket = var.base_bucket_name
     key    = "state/terraform/terraform.tfstate"
-    region = "${var.aws_s3_bucket}"
+    region = var.aws_region
   }
 }
