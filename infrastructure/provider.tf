67519bfc9b181a28646b860dcef1a935cf7c1c66
elnataoliveira <elnataoliveira@gmail.com>
2021-08-15 11:10:37 -0300

    deleted old files and edit variables

---
+++ infrastructure/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = var.aws_region
+
+}
+
+#controle de estado 
+terraform {
+  backend "s3" {
+    bucket = var.base_bucket_name
+    key    = "state/terraform/terraform.tfstate"
+    region = var.aws_s3_bucket
+  }
+}
